46e2d7dcb47853e1abc5ad36bdf749bc949e16f5
jonfaile <jonfraile82@gmail.com>
2016-09-22 10:59:49 +0200

    Pendiente Procesos almacenados

---
+++ scriptBBDD-GestionCandidatos.sql
@@ -0,0 +1,33 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.6.17 - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             9.3.0.5027
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Volcando estructura de base de datos para gc
+CREATE DATABASE IF NOT EXISTS `gc` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `gc`;
+
+
+-- Volcando estructura para tabla gc.candidatos
+CREATE TABLE IF NOT EXISTS `candidatos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` varchar(10) DEFAULT NULL,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `dni` (`dni`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla gc.candidatos: ~0 rows (aproximadamente)
+DELETE FROM `candidatos`;
+/*!40000 ALTER TABLE `candidatos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `candidatos` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
