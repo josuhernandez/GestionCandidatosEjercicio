5d393bd6466ff6c30c71699e745a79904d5fddd0
jonfaile <jonfraile82@gmail.com>
2016-09-22 12:09:17 +0200

    Ok

---
+++ src/main/java/com/ipartek/formacion/repository/CandidatoDAO.java
@@ -0,0 +1,22 @@
+package com.ipartek.formacion.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.ipartek.formacion.domain.Candidato;
+
+public interface CandidatoDAO extends Serializable, DAOSetter {
+
+	List<Candidato> getAll();
+
+	Candidato getById(long id);
+
+	Candidato getByDni(String dni);
+
+	boolean eliminar(long id);
+
+	boolean insertar(Candidato candidato);
+
+	boolean modificar(Candidato candidato);
+
+}
